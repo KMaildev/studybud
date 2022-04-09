a5fbe953da0df1ae1665fddfb744bbd6b8ac9ec6
KMaildev <kmailsoftwaredevelopment@gmail.com>
2022-04-10 01:32:00 +0630

    first commit

---
+++ base/views.py
@@ -0,0 +1,10 @@
+from http.client import HTTPResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def room(request):
+    return render(request, 'room.html')
