7ce48252114890ea2995ced93994b54a118511cc
KMaildev <kmailsoftwaredevelopment@gmail.com>
2022-04-10 01:38:52 +0630

    Update

--- base/views.py
+++ base/views.py
@@ -1,9 +1,18 @@
 from http.client import HTTPResponse
+from multiprocessing import context
 from django.shortcuts import render
 
 
+rooms = [
+    {'id': 1, 'name': 'Python'},
+    {'id': 2, 'name': 'Django'},
+    {'id': 3, 'name': 'PHP'},
+]
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context )
 
 
 def room(request):
